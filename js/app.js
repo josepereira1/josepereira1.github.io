f44115a23df44a27cccef446fe4bd511db61481a
josepereira1 <joseandremp1@gmail.com>
2020-08-01 18:43:18 +0100

    fix bug on projects pagination

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-var maxProjectsPerPage = 6
+const githubUsername = 'josepereira1'
+var maxProjectsPerPage = 5
 var lastPage
 var numberPages
 var jsonData = {}
-var control = 1
 var downloadIcon = '<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cloud-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/><path fill-rule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/></svg>'
 
-//  used to not spend unnecessary requests in development
-var dev = new Boolean(false)
+//  birthday here to calculate age
+const year = 1997, month = 6, day = 19;
+
+//  used to not spend unnecessary requests in development (aboute me information, projects requested to github, because exists max limit of requests)
+var dev = false
 
 function getProjectsByPage(page){
 
@@ -18,9 +21,8 @@ function getProjectsByPage(page){
 
     if(!jsonData || jsonData.length == 0){
         $(document).ready(function(){
-            $.get("https://api.github.com/users/josepereira1/repos") .done(function(data) {
-                console.log("GET https://api.github.com/users/josepereira1/repos")
-                control = 50
+            $.get('https://api.github.com/users/' + githubUsername + '/repos') .done(function(data) {
+                console.log('GET https://api.github.com/users/' + githubUsername + '/repos')
                 jsonData = data
             }).fail(function(){
                 controlFlag = 0
@@ -34,8 +36,10 @@ function getProjectsByPage(page){
     if(controlFlag == 1 || jsonData.length > 0){
         let content = ""
     
-        minIndex = (page-1)*maxProjectsPerPage
-        maxIndex = (page-1)*maxProjectsPerPage + (maxProjectsPerPage-1)
+        let minIndex = (page-1)*maxProjectsPerPage
+        let maxIndex = (page-1)*maxProjectsPerPage + (maxProjectsPerPage-1)
+
+        if(maxIndex > (jsonData.length -1)) maxIndex = (jsonData.length -1)
 
         $('#projectTableContent').html("")
         $("#pag" + page).addClass("disabled")
@@ -84,12 +88,19 @@ function numberPages(page){
     if(page <= 0 || page > numberPages) return
 
     $(document).ready(function(){
-        $.get("https://api.github.com/users/josepereira1/repos") .done(function(data) {
-            console.log("GET https://api.github.com/users/josepereira1/repos")
+        $.get('https://api.github.com/users/' + githubUsername + '/repos') .done(function(data) {
+            console.log('GET https://api.github.com/users/' + githubUsername + '/repos')
             jsonData = data
             let content = ""
 
-            numberPages = Math.round(jsonData.length / maxProjectsPerPage);
+            console.log(jsonData)
+
+            let decimalValue = (jsonData.length / maxProjectsPerPage)
+            let constValue = Math.round(decimalValue)
+            
+            //  this if is needed, because, if the result is 4.1 I want to numberPages = 5
+            if(decimalValue > constValue) numberPages = constValue + 1
+            else numberPages = constValue
 
             $('#pagination').append('<li id="back" class="waves-effect disabled"><a id="linkBack" href="#!"><i class="material-icons">chevron_left</i></a></li>')
 
@@ -130,24 +141,31 @@ function numberPages(page){
 }
 
 function calculateAge() {
-    let year = 1997, month = 6, day = 19, hour = 14, min = 15;
-    var ageDifMs = Date.now() - new Date(year, month, day, hour, min, 0, 0);
+    //  you could specify hour and minutes
+    var ageDifMs = Date.now() - new Date(year, month, day, 0, 0, 0, 0);
     var ageDate = new Date(ageDifMs);
     $('#age').html(Math.abs(ageDate.getUTCFullYear() - 1970) + ' years')
 }
 
 function aboutme(){
+    if(dev === true) return ;
+
     $(document).ready(function(){
-        $.get("https://api.github.com/users/josepereira1") .done(function(data) {
-            console.log("GET https://api.github.com/users/josepereira1")
+        $.get('https://api.github.com/users/' + githubUsername) .done(function(data) {
+            console.log('GET https://api.github.com/users/' + githubUsername)
             var profile = data
             
-            $('#location').append(profile.location)
-            $('#publicrepos').append(profile.public_repos)
-            $('#name').append(profile.name)
+            if(profile.location != '')$('#location').append(profile.location)
+            else $('#location').append('Portugal')
+            
+            if(profile.public_repos != '' && profile.public_repos != 0)$('#publicrepos').append(profile.public_repos)
+            else $('#publicrepos').append('---')
+
+            if(profile.name && profile.name != '')$('#name').append(profile.name)
+            else $('#name').append('José Pereira')
             
         }).fail(function(){
-            $('#location').append('---')
+            $('#location').append('Portugal')
             $('#publicrepos').append('---')
             $('#name').append('José Pereira')
         })
@@ -163,8 +181,8 @@ function skills(){
 
     //  in development
     $(document).ready(function(){
-        $.get("https://api.github.com/users/josepereira1/repos") .done(function(data) {
-            console.log("GET https://api.github.com/users/josepereira1/repos")
+        $.get('https://api.github.com/users/' + githubUsername + '/repos') .done(function(data) {
+            console.log('GET https://api.github.com/users/' + githubUsername + '/repos')
             jsonData = data
             var skills = {}
 
