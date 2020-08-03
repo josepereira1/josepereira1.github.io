55d66961742f59ae9178c16042d0009fcbdf2d16
josepereira1 <joseandremp1@gmail.com>
2020-08-03 15:58:29 +0100

    refactoring and profile picture adapted to different screen sizes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
+//  constants
+
 const githubUsername = 'josepereira1'
-var maxProjectsPerPage = 5
-var lastPage
-var numberPages
-var githubProjectsJson = {}
-var downloadIcon = '<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cloud-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/><path fill-rule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/></svg>'
 //  birthday here to calculate age
 const year = 1997, month = 6, day = 19;
 
+//  github picture
+const profilePicture = 'https://avatars1.githubusercontent.com/u/31769547?v=4'
+
+//  linkedin picture
+//const profilePicture = 'https://media-exp1.licdn.com/dms/image/C5603AQG8Ot6adcM2IA/profile-displayphoto-shrink_200_200/0?e=1601510400&v=beta&t=IDbX5xGa9xt9Jk6LkcQUpOWvR9GQ0WwQcM-cl85u5II'
+
+const downloadIcon = '<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cloud-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/><path fill-rule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/></svg>'
+const maxProjectsPerPage = 5
+
 //  used to not spend unnecessary requests in development (aboute me information, projects requested to github, because exists max limit of requests)
-var dev = false
+const dev = false
+
+//  --------------------------------------------------------------
+
+//  variables
+var lastPage
+var numberPages
+var githubProjectsJson = {}
+
 
 /**
  * load information necessary to create projects table
  * @param {String} comparator comparator is the sort method (options: created, updated, pushed, full_name)
  */
 function loadProjects(comparator){
-    console.log('loadProjects')
     if(dev === true) return ;
 
     $(document).ready(function(){
@@ -53,8 +66,6 @@ function parseDate(dateFormat){
  * @param {Number} page actual page
  */
 function createProjectsTableAndPaginationOfPage(page){
-    console.log('createProjectsTableAndPaginationOfPage')
-    
     if(dev === true || page <= 0 || page > numberPages) return 
 
     if(Object.keys(githubProjectsJson).length === 0){
@@ -109,7 +120,6 @@ function updatePagination(actualPage){
     }
 }
 function calculateNumberPages(){
-    console.log('calculateNumberPages')
     let decimalValue = (githubProjectsJson.length / maxProjectsPerPage)
     let constValue = Math.round(decimalValue)
     let tmp = 0
@@ -121,7 +131,6 @@ function calculateNumberPages(){
 }
 
 function createPagination(numberPages, page){
-    console.log('pagination')
     $('pagination').html('')
     $('#pagination').append('<li id="back" class="waves-effect disabled"><a id="linkBack" href="#!"><i class="material-icons">chevron_left</i></a></li>')
 
@@ -135,7 +144,6 @@ function createPagination(numberPages, page){
 }
 
 function calculateAge() {
-    console.log('calculateAge')
     //  you could specify hour and minutes
     var ageDifMs = Date.now() - new Date(year, month, day, 0, 0, 0, 0);
     var ageDate = new Date(ageDifMs);
@@ -143,7 +151,6 @@ function calculateAge() {
 }
 
 function aboutme(){
-    console.log('aboutme')
     if(dev === true) return ;
 
     $(document).ready(function(){
@@ -215,4 +222,24 @@ function skills(){
             $("#projects").html("")
         })
     });
+}
+
+$(window).on("resize", function(){
+    adjustProfileImage()
+});
+
+$(window).on("load", function(){
+    adjustProfileImage()
+});
+
+function adjustProfileImage(){
+    if(window.visualViewport.width < 1400){
+        $('#about-me-img').html('')
+        $('#about-me-table').removeClass('s8')
+        $('#about-me-table').addClass('s12')
+    }else{
+        $('#about-me-img').html('<img style="display: block;margin-left: auto;margin-right: auto;width: 50%;border-radius: 8px;" src="' + profilePicture + '">')
+        $('#about-me-table').addClass('s8')
+        $('#about-me-table').removeClass('s12')
+    }
 }
