58e3c3b055f5de20de7b95b0cfc38c4d539ec90c
josepereira1 <joseandremp1@gmail.com>
2020-08-05 03:12:19 +0100

    resized image

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ const githubUsername = 'josepereira1'
 const year = 1997, month = 6, day = 19;
 
 //  github picture
-const profilePicture = 'https://avatars1.githubusercontent.com/u/31769547?v=4'
+//const profilePicture = 'https://avatars1.githubusercontent.com/u/31769547?v=4'
 
 //  linkedin picture
-//const profilePicture = 'https://media-exp1.licdn.com/dms/image/C5603AQG8Ot6adcM2IA/profile-displayphoto-shrink_200_200/0?e=1601510400&v=beta&t=IDbX5xGa9xt9Jk6LkcQUpOWvR9GQ0WwQcM-cl85u5II'
+const profilePicture = 'https://media-exp1.licdn.com/dms/image/C5603AQG8Ot6adcM2IA/profile-displayphoto-shrink_200_200/0?e=1601510400&v=beta&t=IDbX5xGa9xt9Jk6LkcQUpOWvR9GQ0WwQcM-cl85u5II'
 
 const downloadIcon = '<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cloud-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/><path fill-rule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/></svg>'
 const maxProjectsPerPage = 5
@@ -189,7 +189,7 @@ function adjustProfileImage(){
         $('#about-me-table').removeClass('s8')
         $('#about-me-table').addClass('s12')
     }else{
-        $('#about-me-img').html('<img style="display: block;margin-left: auto;margin-right: auto;width: 50%;border-radius: 8px;" src="' + profilePicture + '">')
+        $('#about-me-img').html('<img style="display: block;margin-left: auto;margin-right: auto;width: 65%;border-radius: 5px;" src="' + profilePicture + '">')
         $('#about-me-table').addClass('s8')
         $('#about-me-table').removeClass('s12')
     }
