5c8850ca5d8d8bf067fc9dee38fe293cb6a8cda7
josepereira1 <joseandremp1@gmail.com>
2022-07-17 12:44:46 +0100

    melhorias no cálculo da idade

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 //  constants
 
 const githubUsername = 'josepereira1'
+
 //  birthday here to calculate age
-const year = 1997, month = 6, day = 19;
+const birthday = '1997-06-19';
 
 //  linkedin picture
-const profilePicture = 'https://media-exp1.licdn.com/dms/image/C5603AQG8Ot6adcM2IA/profile-displayphoto-shrink_200_200/0/1572694184789?e=1626307200&v=beta&t=dOCh6E3d1G9l9vUkg6amkAcAhkzXw4hTX2XSRkVeqec'
+const profilePicture = 'https://media-exp2.licdn.com/dms/image/C5603AQG8Ot6adcM2IA/profile-displayphoto-shrink_200_200/0/1572694184789?e=1663804800&v=beta&t=1zOv0buIVPuaTIe7VRFhtd_Hnl0jFrOtI0hvri_uAi8'
 
 const downloadIcon = '<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cloud-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/><path fill-rule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/></svg>'
 const maxProjectsPerPage = 6
@@ -142,12 +143,9 @@ function createPagination(numberPages, page){
 }
 
 function calculateAge() {
-    //  you could specify hour and minutes
-    var ageDifMs = Date.now() - new Date(year, month, day, 0, 0, 0, 0);
-    var ageDate = new Date(ageDifMs);
-    var age = Math.abs(ageDate.getUTCFullYear() - 1970)
-    updateLinkDescription(age)
-    updateAboutMeAge(age)
+    var age = howOld(birthday)
+    updateLinkDescription(age.years)
+    updateAboutMeAge(age.years)
 }
 
 function updateLinkDescription(age){
@@ -274,3 +272,60 @@ function skills(){
         })
     });
 }
+
+function howOld(date) {  
+    var dob = new Date(date);
+
+    console.log(dob)
+
+    if(date==null || date==''){  
+      document.getElementById("result").innerHTML = "Choose a date please!";    
+      return false;   
+    } else {  
+        var dobYear = dob.getYear();  
+        var dobMonth = dob.getMonth();  
+        var dobDate = dob.getDate();  
+        var now = new Date();  
+        var currentYear = now.getYear();  
+        var currentMonth = now.getMonth();  
+        var currentDate = now.getDate();  
+        var age = {};  
+        var ageString = "";  
+        yearAge = currentYear - dobYear;
+
+        //console.log(currentDate)
+
+        if(currentYear < dobYear 
+            || (currentYear == dobYear && currentMonth < dobMonth) 
+            || (currentYear == dobYear && currentMonth == dobMonth && currentDate < dobDate)) {
+                document.getElementById("result").innerHTML = "Enter a date earlier than the current one.";
+                return false;
+            }
+
+        if(currentYear == dobYear && currentMonth < dobMonth) return false;
+
+        if (currentMonth >= dobMonth)  
+            var monthAge = currentMonth - dobMonth;  
+        else {  
+            yearAge--;  
+            var monthAge = 12 + currentMonth - dobMonth;  
+        }
+        if (currentDate >= dobDate)  
+            var dateAge = currentDate - dobDate;  
+        else {  
+            monthAge--;  
+            var dateAge = 31 + currentDate - dobDate;  
+        
+            if (monthAge < 0) {  
+                monthAge = 11;  
+                yearAge--;  
+            }  
+        }  
+        age = {  
+            years: yearAge,  
+            months: monthAge,  
+            days: dateAge  
+        };
+        return age
+  }  
+}  
